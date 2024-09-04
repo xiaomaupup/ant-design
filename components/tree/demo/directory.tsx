6a452563030eef3d26e3a3e71a141a73abf310f0
xiaomaupup <yuetingmatt@126.com>
2024-09-04 14:33:26 +0800

    Update directory.tsx
    Signed-off-by: xiaomaupup <yuetingmatt@126.com>

--- components/tree/demo/directory.tsx
+++ components/tree/demo/directory.tsx
@@ -11,7 +11,7 @@ const treeData: TreeDataNode[] = [
     title: 'parent 0',
     key: '0-0',
     children: [
-      { title: 'leaf 0-0', key: '0-0-0', isLeaf: true },
+      { title: '手术史', key: '0-0-0', isLeaf: true },
       { title: 'leaf 0-1', key: '0-0-1', isLeaf: true },
     ],
   },
